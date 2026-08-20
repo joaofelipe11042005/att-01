b668f1dc3217229249808104a6271893a1ad3a63
048690 <048690@UNI-LAB3-11>
2026-08-19 21:22:48 -0300

    Adicionar arquivos de projeto.

---
+++ att h1/Controllers/h1.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MinhaApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PessoaController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CalcularIMC(string nome, double peso, double altura)
+        {
+            double imc = peso / (altura * altura);
+
+            return Ok(new
+            {
+                Nome = nome,
+                Peso = peso,
+                Altura = altura,
+                IMC = imc
+            });
+        }
+    }
+}
